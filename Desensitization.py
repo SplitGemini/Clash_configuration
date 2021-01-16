aa7c8ee42cee72ac1d9ddffc1c023a629b3922b7
SplitGemini <764402868@qq.com>
2021-01-16 10:25:11 +0800

    update

--- Desensitization.py
+++ Desensitization.py
@@ -7,37 +7,26 @@ import os
 # HOME Directory path
 path = "../"
 # files name
-js = "parser.js"
 yaml = "parser.yaml"
 setting = "cfw-settings.yaml"
 
+
 # patterns
 user = (r"(C:(\\\\|\\|/)Users(\\\\|\\|/))[^\\/]*(?=(\\\\|\\|/)\.config)",r"\1YOURNAME")
-id = ("[0-9a-z]{32}","")
-token = ("[0-9a-z]{40}","")
-group = ("(?<=const fileNames = \{).*(?=\})","\"v2ray\":\"clash\"")
 # 删除被'##delete start'和'##end'包围的内容
 delete = (r"\s*##\s?delete\s?start[\s\S]*?##\s?(delete\s?)?end","")
 
-# js
-with open(os.path.join(path, js), "r", encoding="utf-8") as f:
-    result = re.sub(id[0], id[1], f.read(), count=1, flags=0)
-    result = re.sub(token[0], token[1], result, count=1, flags=0)
-    result = re.sub(group[0], group[1], result, count=1, flags=0)
-    result = re.sub(user[0], user[1], result, count=0, flags=0)
-with open(js, "w", encoding="utf-8") as f:
-    f.write(result)
-
-# yaml
+# parser.yaml
 with open(os.path.join(path, yaml), "r", encoding="utf-8") as f:
     result = re.sub(delete[0], delete[1], f.read(), count=0, flags=0)
 with open(yaml, "w", encoding="utf-8") as f:
     f.write(result)
 
-# setting
+# cfw-settings.yaml
 with open(os.path.join(path, setting), "r", encoding="utf-8") as f:
     result = re.sub(user[0], user[1], f.read(), count=0, flags=0)
     result = re.sub(delete[0], delete[1], result, count=0, flags=0)
 with open(setting, "w", encoding="utf-8") as f:
     f.write(result)
+
 print("Desensitization finished...")
--- README.md
+++ README.md
@@ -8,6 +8,7 @@ mixin功能大概只能用来替换dns，虽然也可以替换规则，但是无
 - 将订阅结构完全统一化，订阅自带的规则组和规则完全丢弃而使用自己收集的（MyRules），自定义节点和规则在parser.yaml中添加 
 - 添加根据profiles名称关键词自动上传私密gist，每次更新自动上传，可以通过log查看订阅链接或调试信息，可以用于 Clash For Android 等进行远程订阅，注意gist id链接不可泄露
 - 上传gist中添加dns设置以配合Adgurad
+- 自动分组和自动签到功能，by [https://github.com/yi-Xu-0100/cfw-scripts](https://github.com/yi-Xu-0100/cfw-scripts)
 
 ## 部署
 
@@ -15,8 +16,9 @@ mixin功能大概只能用来替换dns，虽然也可以替换规则，但是无
 1. [UnblockNeteaseMusic](https://github.com/cnsilvan/UnblockNeteaseMusic) ，作为子程序，放在cfw配置目录，用于本地解锁网易云音乐灰色歌曲
 1. 手动创建好一个gist，parser.js 中设置log filename路径，gistId修改为已创建好的gist的id；'token'修改为 `Personal Access Token`（[在此创建](https://github.com/settings/tokens/new?scopes=gist&description=Subconverter)），注意token获取后如没有保存将不再可见。
 1. 修改关键词字典，配置名包含关键词则将其以对应关键词字典值为文件名上传到gist
+1. 修改variables.yaml 修改参数实现自动签到自动分组等功能
 1. cfw-settings.yaml替换全部`YOURNAME`为你电脑用户名，或者将带该标记的目录修改为cfw配置目录
-1. 复制parser.js、parser.yaml、cfw-settings.yaml、subconverter文件夹到cfw配置目录，安装版为“C:/Users/YOURNAME/.config/clash”
+1. 复制parser.js、parser.yaml、cfw-settings.yaml、auto-check-in.js、variables.yaml、subconverter文件夹到cfw配置目录，安装版为“C:/Users/YOURNAME/.config/clash”
 
 ## 参照
 
